df1f585eab50a29031dca5b8837e9cf8727a5e2f
Felipe Rodrigues Santos <feliperodrigues.dw@gmail.com>
2022-07-14 13:23:13 -0300

    Classes gerente e vendedor adicionadas
    Classes gerente e vendedor que herdam Funcionário foram criadas

---
+++ LojaChocolateAppView/LojaChocolateApp/Model/Gerente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaChocolateApp.Model
+{
+    public class Gerente : Funcionario
+    {
+        public Gerente(int id, string nome, string cpf, string contato, decimal salario, string cargo, string dataCadastro) : base(id, nome, cpf, contato, salario, cargo, dataCadastro)
+        {
+        }
+    }
+}
